2d15d09332edae5a57d127233bd5ea73be7ce65d
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-16 20:05:32 +0200

    script 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Display with functions listed in NAME
+ *
+ * Return: 1 only this time
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
