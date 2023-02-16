f98a7d686a6c6c158665912c77f9ca7cac21f0aa
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-16 19:04:16 +0200

    C-code 1.1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
 * main - Entry point
 * Description: prints a Message
-8 Return: 0
+* Return: 0
 */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
