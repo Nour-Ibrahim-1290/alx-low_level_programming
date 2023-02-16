6734b91602a96103f3409520f5f1077ccd02973d
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-16 19:02:46 +0200

    C-code 1

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints a Message
+8 Return: 0
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
