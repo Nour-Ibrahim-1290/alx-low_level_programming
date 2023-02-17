d157fd1c472fb15b3e5a7b47de2813916ae42ec3
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-17 18:39:23 +0200

    C-code 1 -- Check last digit on int

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Extract the last digit of a number
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	/* code given by question */
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* my code/solution */
+	int last = n % 10;
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if ((last < 6) && (last != 0))
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+
+
+	return (0);
+}
