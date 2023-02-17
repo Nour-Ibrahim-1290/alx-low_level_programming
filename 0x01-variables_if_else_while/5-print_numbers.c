76153ad2bb5492417db07a03ca0b401e1b6f879d
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-17 19:39:08 +0200

    C-code 5-- Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: display all numbers of base 10
+ *
+ * Return: Alawys zero
+ */
+
+int main(void)
+{
+
+	/* Lowercase Alphabet */
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+
+	/* New Line */
+	printf("\n");
+
+	return (0);
+}
