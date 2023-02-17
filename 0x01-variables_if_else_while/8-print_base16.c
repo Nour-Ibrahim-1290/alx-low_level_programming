90813bbb99c0aafcebcdaa03226d2f62ca04435d
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-17 20:17:31 +0200

    C-code 8 -- Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: display all numbers of base 16
+ *
+ * Return: Alawys zero
+ */
+
+int main(void)
+{
+
+	/* Lowercase Alphabet */
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar((char)i);
+
+	for (i = 97; i <= 103; i++)
+		putchar((char)i);
+
+	/* New Line */
+	putchar('\n');
+
+	return (0);
+}
