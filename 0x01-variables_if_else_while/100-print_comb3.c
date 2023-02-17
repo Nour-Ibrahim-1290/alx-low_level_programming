a6738d06c559687948ed80334c4c685de1cff30b
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-17 22:03:29 +0200

    C-code 100 -- 2 digit nums

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print 2 digit number combinations
+ *
+ * Return: Alawys zero
+ */
+
+int main(void)
+{
+
+	/* Lowercase Alphabet */
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i+1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			
+			if ((char)i != '8' || (char)j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
