a3db135171d3844fba86f8d8ce0902eb7ad30bc8
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-17 18:43:47 +0200

    C-code 1.1 -- Check last digit on int

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,13 @@ int main(void)
 {
 	/* code given by question */
 	int n;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* my code/solution */
-	int last = n % 10;
+	last = n % 10;
 	if (last > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	else if (last == 0)
