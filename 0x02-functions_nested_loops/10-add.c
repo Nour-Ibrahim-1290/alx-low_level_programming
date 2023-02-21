996184bcc9d8d943d565a1b57b979cfd86af18ae
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-21 19:53:52 +0200

    C-code 10 -- Add ints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry Point
+ *
+ * Description: add two integers
+ *
+ * @a: first integer to add
+ * @b: 2nd integer to add
+ *
+ * Return: int value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
