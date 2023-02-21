d5c1807d40db8ce30352d65785e2b5f166642090
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-21 14:19:51 +0200

    C-code 3 -- Check if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Entry Point
+ *
+ * Description: Check whether a letter is lowercase
+ *
+ * @c: integer value of char to check whether
+ * lowercase letter or not
+ *
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
