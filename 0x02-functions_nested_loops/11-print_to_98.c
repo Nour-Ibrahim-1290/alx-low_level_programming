6bedb55170340b8524ac45d86f6c5711900d1571
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-21 20:20:42 +0200

    C-code 11 -- To 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Entry Point
+ *
+ * Description: Printing Numbers from n to 98
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
+		}
+	else
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
+		}
+}
