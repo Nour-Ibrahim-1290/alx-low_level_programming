197f081eeb79d6d9cce4d5d3abd7ce52430e6e87
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-21 17:38:32 +0200

    C-code 8 -- Loooong day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer: Entry Point
+ *
+ * Description: Nested Looping
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 4; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
