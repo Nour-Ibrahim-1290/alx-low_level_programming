73d0d7c5cc2474b5c6f6e82f89061aab2bde5787
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-21 13:44:05 +0200

    C-code 2 -- Lowercase letters 10 times over

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry Point
+ *
+ * Description: write all lowercase alphabet using
+ * only _putchar twice
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+
+		_putchar('\n');
+	}
+}
