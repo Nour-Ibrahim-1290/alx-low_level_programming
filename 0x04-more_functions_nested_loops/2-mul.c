7e8dd97653f2b529404e2eedf28d602be6024dc1
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-23 18:22:43 +0200

    C-code 2 - Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ *
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: multiplication of a, and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
