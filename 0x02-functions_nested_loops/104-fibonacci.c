35efb89c8115eeed93822d25994b99c15f500e4f
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-23 04:02:27 +0200

    C-code 16 - Fibonacci to 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Alawys 0
+ */
+
+int main(void)
+{
+	unsigned long a, b;
+	int i;
+
+	a = 1;
+	b = 2;
+	printf("%lu, %lu, ", a, b);
+
+	for (i = 3; i < 98; i++)
+	{
+		b += a;
+		a = b - a;
+		
+		printf("%lu, ", b);
+	}
+
+	b += a;
+	printf("%lu\n", b);
+
+	return (0);
+}
