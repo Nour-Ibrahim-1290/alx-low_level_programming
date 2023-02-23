4eae8ee513710eed7dd9ae485f147e82ec480810
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-23 03:34:48 +0200

    C-code 14 - long not int

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,21 +8,25 @@
 
 int main(void)
 {
-	int f[50];
+	long f[50];
 	int i;
 
 	f[0] = 1;
 	f[1] = 2;
-	printf("%d, %d, ", f[0], f[1]);
+	printf("%ld, %ld, ", f[0], f[1]);
 
 	for (i = 2; i < 49; i++)
 	{
-		f[i] = f[i-1] + f[i-2];
-		printf("%d, ", f[i]);
+		f[i] = f[i - 1] + f[i - 2];
+		if (f[i] < 0)
+		{
+			f[i] *= -1;
+		}
+		printf("%ld, ", f[i]);
 	}
 
 	f[49] = f[48] + f[47];
-	printf("%d\n", f[49]);
+	printf("%ld\n", f[49]);
 
 	return (0);
 }
