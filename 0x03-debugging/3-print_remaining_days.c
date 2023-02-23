e0acbb58ddca73ad0c2df2df5c21f71a6036c874
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-24 00:34:35 +0200

    C-code 3 - Changing Style

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,19 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0)
+	if (((year % 100 == 0) && (year % 400 == 0)) || (year % 4 == 0))
 	{
-		if (year % 100 != 0)
+		if (month > 2 && day >= 60)
 		{
-			if (month >= 2 && day >= 60)
-				day++;
+			day++;
 		}
-		else
-		{
-			if (year % 400 == 0)
-				if (month >= 2 && day >= 60)
-					day++;
-		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
