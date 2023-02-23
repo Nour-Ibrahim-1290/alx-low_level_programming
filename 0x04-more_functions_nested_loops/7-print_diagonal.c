25dcb3c74387b5f0b68c4eea1e9567d8a643a638
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-23 19:13:12 +0200

    C-code 7 -- Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints chracter '\' n times
+ *
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			for (i = 0; i < j; i++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
