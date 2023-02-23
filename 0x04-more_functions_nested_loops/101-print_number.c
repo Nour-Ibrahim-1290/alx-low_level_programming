5d95641734bd10376b65c8a5a09a27bfb2e98946
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-23 22:38:08 +0200

    C-code 101 - recrusion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_number - print integer with _putchar only
@@ -11,42 +10,16 @@
 
 void print_number(int n)
 {
-	int ones, tens, hunds, thous;
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
-		_putchar('-');
-		n = n * -1;
+		_putchar(45);
+		x = -n;
 	}
+	if (x / 10)
+		print_number(x / 10);
 
-	ones = n % 10;
-
-	if (n >= 1000)
-	{
-		tens = ((n % 100) - ones) / 10;
-		hunds = ((n % 1000) - tens - ones) / 100;
-		thous = n / 1000;
-		_putchar(thous + '0');
-		_putchar(hunds + '0');
-		_putchar(tens + '0');
-		_putchar(ones + '0');
-	}
-	else if (n >= 100)
-	{
-		tens = ((n % 100) - ones) / 10;
-		hunds = n / 100;
-		_putchar(hunds + '0');
-		_putchar(tens + '0');
-		_putchar(ones + '0');
-	}
-	else if (n >= 10)
-	{
-		tens = n / 10;
-		_putchar(tens + '0');
-		_putchar(ones + '0');
-	}
-	else
-	{
-		_putchar(ones + '0');
-	}
+	_putchar((x % 10) + '0');
 }
