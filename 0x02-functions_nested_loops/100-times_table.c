00005a605f608e032708b25b8c3df12b83909fb7
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-23 03:27:38 +0200

    C-code 12 - Refinning Code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,57 +16,8 @@ void higher_n(int n);
 
 void print_times_table(int n)
 {
-	if (n > 0 && n < 10)
-	{
+	if (n > 0 && n < 15)
 		higher_n(n);
-	}
-	else if (n < 15)
-	{
-		higher_n(n);
-	}
-}
-
-
-/**
- * lower_n - print time table when n < 10
- *
- * @n: number of time tables
- *
- * Return: void
- */
-void lower_n(int n)
-{
-
-	int row, col, product, ones, tens;
-		for (row = 0; row <= n; row++)
-		{
-			for (col = 0; col <= n; col++)
-			{
-				product = row * col;
-				ones = product % 10;
-				tens = product / 10;
-
-				if (col == 0)
-				{
-					_putchar('0');
-				}
-				else if (product < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(ones + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(tens + '0');
-					_putchar(ones + '0');
-				}
-			}
-			_putchar('\n');
-		}
 }
 
 
