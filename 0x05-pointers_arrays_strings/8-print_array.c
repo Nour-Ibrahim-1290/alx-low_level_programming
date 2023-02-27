b3f729e10bcd372edb11cb8d26d88ddfe673fd03
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-27 11:58:29 +0200

    Arrays 8 - erro handling

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,9 +16,13 @@ void print_array(int *a, int n)
 	int i;
 
 	if (n > 0)
+	{
 		for (i = 0; i < n - 1; i++)
 			printf("%d, ", a[i]);
 		printf("%d\n", a[n - 1]);
+	}
 	else
+	{
 		printf("\n");
+	}
 }
