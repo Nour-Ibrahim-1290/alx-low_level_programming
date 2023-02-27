2bef100157efa4fd0f52b6e00d61a36608fdeb79
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-27 09:49:09 +0200

    Strings 4 - Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry Point
+ *
+ * Description: prints a string in reverse
+ * @s: string - char array
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len != -1)
+		_putchar(s[len--]);
+
+	_putchar('\n');
+}
