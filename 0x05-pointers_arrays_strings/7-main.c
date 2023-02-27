3d655bdfe6baa24b997885b7784cfb46ff6e2446
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-27 11:32:16 +0200

    Strings 7 - 2nd half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Alawys 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry Point
+ *
+ * Description: prints the 2nd half of a string
+ * @str: string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len, n;
+
+	len = 0;
+
+	/* Extracting the length of string*/
+	while (str[len] != '\0')
+		len++;
+
+	/*Printing the 2nd half*/
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	while (str[n] != '\0')
+		_putchar(str[n++]);
+	_putchar('\n');
+}
