6807b356b11945e1e0d19ff1ed0beec3aa12672a
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-02-27 09:30:54 +0200

    Strings 3 - Puts

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Alawys 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+
+	return (0);
+}
