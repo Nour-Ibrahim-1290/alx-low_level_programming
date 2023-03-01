22dc47288bf953bfac46622bcbd005a2593b70ca
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-01 23:59:48 +0200

    Strings 7 - Correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,14 +15,17 @@ char *leet(char *str)
 	char *ptr = str;
 	int i;
 	char sub[] = {'A', 'E', 'O', 'T', 'L'};
+	char sub2[] = {'a', 'e', 'o', 't', 'l'};
 	char code[] = {'4', '3', '0', '7', '1'};
 
 	while (*str != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			printf("%c, %c\n", *str, *str + 32);
-			if ((*str == sub[i]) || ((*str - 32) == sub[i]))
+			printf("%c, ", *str);
+			printf("\n");
+
+			if (*str == sub[i] || *str == sub2[i])
 			{
 				*str = code[i];
 				break;
