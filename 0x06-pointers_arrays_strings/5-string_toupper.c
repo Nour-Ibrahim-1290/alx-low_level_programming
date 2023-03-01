53b2b00cef680c7cafdff7ec5b19d3aab4328aa6
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-01 19:13:59 +0200

    Strings 5 - Upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Entry Point
+ *
+ * Description: Change all lowercase letters into uppercase
+ * @str: string to change
+ *
+ * Return: pointer to string
+ */
+
+char *string_toupper(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str <= 'z' && *str >= 'a')
+			*str = *str - 32;
+		str++;
+	}
+
+	return (str);
+}
