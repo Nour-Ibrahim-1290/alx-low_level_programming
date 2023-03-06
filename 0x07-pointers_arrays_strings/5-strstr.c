2f8cf4da2972a1e6df5ed3800bf6232541b3b8a0
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-06 12:58:59 +0200

    Strings 5 - strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * match - match s2 from a specific beginning of s1
+ * @s1: string to match
+ * @s2: string to match with
+ *
+ * Return: int
+ */
+
+int match(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (0);
+		s1++;
+		s2++;
+	}
+
+	return (*s2 == '\0');
+}
+
+/**
+ * _strstr - Entry Point
+ *
+ * Description: Find the first occurance of the substring
+ * needle in the string haystack.
+ * @heystack: string to search in
+ * @needle: substring to find
+ *
+ * Return: pointer to substring
+ */
+
+char *_strstr(char *heystack, char *needle)
+{
+	while (*heystack != '\0')
+	{
+		if ((*heystack == *needle) && match(heystack, needle))
+			return (heystack);
+		heystack++;
+	}
+
+	return (NULL);
+}
