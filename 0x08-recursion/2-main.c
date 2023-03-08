dfa15f8e11dda19fd8c913513dfb232cccdc6963
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-08 19:16:23 +0200

    Strings Rescursion - 2

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Alawys 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+
+	return (0);
+}
