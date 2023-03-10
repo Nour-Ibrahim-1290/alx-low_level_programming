59e1e6a3039a4eb2f6ba43628f1f151f9ecad6a0
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-10 13:55:22 +0200

    Betty Styling Fix

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Nour
+ * Desc: Header file contaning the prototypes of all functions
+ *	included in the static library libmy.a
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +28,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
