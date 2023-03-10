9c27fc2f789d8946983397a62dfda811c8bfc194
Nour m. Ibrahim <nouribrahim1290@gmail.com>
2023-03-10 04:39:38 -0800

    Fix

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be
+	_puts("\"At the end of the day, my goal was to be\
 			the best hacker\"\n\t- kevin Mitnick");
 	return (0);
 }
