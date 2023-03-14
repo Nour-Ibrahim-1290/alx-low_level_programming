e4fbfb441e1801c1af2f2448093771bee4ad57f7
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-15 01:52:17 +0200

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - allocate an int 2D array space in memory
+ * with 0 as default value
+ *
+ * @w: width of array
+ * @h: height of array
+ *
+ * Return: Pointer to the allocated space  or NULL if it fails
+ */
+
+int **alloc_grid(int w, int h)
+{
+	int **arr;
+	int i, j;
+
+	if ((w <= 0) || (h <= 0))
+		return (NULL);
+
+	arr = (int **) malloc(h * sizeof(int *));
+
+	for (i = 0; i < h; i++)
+		arr[i] = (int *) malloc(w * sizeof(int));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < h; i++)
+		for (j = 0; j < w; j++)
+			arr[i][j] = 0;
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/3-main.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the addresso of the 2 dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: height of the grid
+ */
+
+void print_grid(int **grid, int width, int height)
+{
+	int w;
+	int h;
+
+	h = 0;
+	while (h < height)
+	{
+		w = 0;
+		while (w < width)
+		{
+			printf("%d ", grid[h][w]);
+			w++;
+		}
+		printf("\n");
+		h++;
+	}
+}
+
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Alawys 0.
+ */
+
+int main(void)
+{
+	int **grid;
+
+	grid = alloc_grid(6, 4);
+	if (grid == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	print_grid(grid, 6, 4);
+	printf("\n");
+	grid[0][3] = 98;
+	grid[3][4] = 402;
+	print_grid(grid, 6, 4);
+
+	return (0);
+}
