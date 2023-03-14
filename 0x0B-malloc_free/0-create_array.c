dcc1cf66b566a7a311144b126d5344a86f71982f
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-14 17:39:43 +0200

    Task0-Fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *arr = (char *) malloc(sizeof(char) * size);
 	unsigned int i;
 
-	if (size == 0)
+	if ((size == 0) || (arr == NULL))
 		return (NULL);
 
 	for (i = 0; i < size; i++)
