1f0f041403121686056d1787bd84b92177c40c09
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-16 15:32:02 +0200

    Task 1 Fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		concat[i] = s1[i];
 
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && s2[j]; j++)
 		concat[i + j] = s2[j];
 
 	concat[size] = '\0';
