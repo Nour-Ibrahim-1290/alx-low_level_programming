c0af9a144d38d0b91fbd3924f96fd58453386594
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-16 15:02:17 +0200

    Initial Commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - check and validate malloc
+ *
+ * @b: size to allocate
+ *
+ * Return: 98 or pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+	
+	if (p == NULL)
+		exit(98);
+	
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Author: Nour M. Ibrahim
+ * Desc: Header file containing the prototypes of all functions
+ *		included in malloc_free dir
+ */
+
+void *malloc_checked(unsigned int b);
+
+
+#endif
