1a3e8a4cdffa590384ee08c91bae338cf4613997
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-21 06:47:19 +0200

    Task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: integer
  * @owner: character string
  */
-struct dog
+typedef struct
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
