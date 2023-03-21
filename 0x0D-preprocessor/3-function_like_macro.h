11a5bb3988e7c6394d176398f0ea08fb50696a19
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-21 03:37:53 +0200

    Task 3 Fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,13 +2,6 @@
 
 #define FUNCTION_LIKE_MACRO_H
 
-/**
- * FILE: 3-function_like_macro.h
- *
- * ABS: function returns abosolute value
- * @x: int to find abs for
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x < 0) ? (-x) : (x))
 
 #endif
