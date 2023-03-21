fd4417b0d07ca3442e931e2cc2a8f7ae11604098
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-21 05:28:45 +0200

    Initial Commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - define a structure of dog charactaristics
+ *
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#endif
