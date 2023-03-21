810b53325c4d8cb32c5e678d810de4003914d51a
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-21 06:08:04 +0200

    struct def fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: integer
  * @owner: character string
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
