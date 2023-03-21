51f14c2a5df68ab706d954f2786ce27993d30eda
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-21 06:20:55 +0200

    Task 1 fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initiate an instant of dog struct
@@ -13,7 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
