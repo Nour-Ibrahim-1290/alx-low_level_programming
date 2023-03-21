177987c40a60a0a45f8c03ea798906ce03041a34
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-21 19:14:57 +0200

    Task 4 fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,25 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * _strlen - find the length of a string
+ * @str: string
+ *
+ * Return: int length
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
 
 /**
  * new_dog - initiate an instant of dog struct
@@ -13,17 +33,41 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t a_dog;
-	dog_t *p_dog;
+	dog_t *a_dog;
+	int nl = 0, ol = 0;
 
-	p_dog = &a_dog;
+	a_dog = malloc(sizeof(struct dog));
+
+	if (a_dog == NULL)
+		return (NULL);
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	p_dog->name = name;
-	p_dog->age = age;
-	p_dog->owner = owner;
+	nl = _strlen(name);
+	ol = _strlen(owner);
 
-	return (p_dog);
+	a_dog->name = malloc(sizeof(char) * nl + 1);
+	if (a_dog->name == NULL)
+	{
+		free(a_dog);
+		return (NULL);
+	}
+
+	a_dog->owner = malloc(sizeof(char) * ol + 1);
+	if (a_dog->owner == NULL)
+	{
+		free(a_dog->name);
+		free(a_dog);
+		return (NULL);
+	}
+
+	while (nl-- >= 0)
+		a_dog->name[nl] = name[nl];
+	a_dog->age = age;
+
+	while (ol-- >= 0)
+		a_dog->owner[ol] = owner[ol];
+
+	return (a_dog);
 }
