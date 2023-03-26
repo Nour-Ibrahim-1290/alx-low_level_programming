b7c07fff0d488db0f1e5259a7dd2ed013f10cf7a
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-26 04:57:26 +0200

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "variadic_functions.h"
+
+
+
+void print_char(va_list);
+void print_int(va_list);
+void print_float(va_list);
+void print_str(va_list);
+void print_all(const char * const, ...);
+
+/**
+ * print_char -print charcater
+ * @arg: va_list argument
+ *
+ * Return: void
+ */
+void print_char(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+
+
+/**
+ * print_int -print integer number
+ * @arg: va_list argument
+ *
+ * Return: void
+ */
+void print_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+
+/**
+ * print_float -print float number
+ * @arg: va_list argument
+ *
+ * Return: void
+ */
+void print_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+
+/**
+ * print_str -print string
+ * @arg: va_list argument
+ *
+ * Return: void
+ */
+void print_str(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (str != NULL)
+		printf("%s", str);
+	else
+		printf("(nil)");
+}
+
+
+/**
+ * print_all - print all the agruments given to function
+ * with the given separator in place
+ *
+ * @format: arguments datatype identifiers
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0, len;
+	va_list arg;
+	d_type data_type[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_str}
+	};
+
+	if (format != NULL)
+	{
+		len = strlen(format);
+		va_start(arg, format);
+
+		while (i < len)
+		{
+			j = 0;
+			while (j < 4 && format[i] != data_type[j].id)
+				j++;
+
+			if (j < 4)
+				data_type[j].f(arg);
+			i++;
+		}
+		printf("\n");
+		va_end(arg);
+	}
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,23 @@
 #ifndef VAR_FUNC_H
 #define VAR_FUNC_H
 
+#include <stdarg.h>
+
+/**
+ * struct d_type - strcuture of pointers to function to
+ * match character with data typr to print
+ * @id: character to match
+ * @f: function to use for printing
+ */
+typedef struct d_type
+{
+	char id;
+	void (*f)(va_list arg);
+} d_type;
+
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
