ca9bd4ffb7bd695790254148288a4850a4371d8d
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-03-31 09:11:44 +0200

    Adv. Task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) before_main(void);
+
+/**
+ * before_main - prints a string before main
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Alawys 0.
+ */
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challanges one a race.)\n");
+	return (0);
+}
