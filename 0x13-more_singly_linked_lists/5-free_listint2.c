e8e8e76bc90a8c6a36ad6df957f58987ea815667
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-04-03 07:25:16 +0200

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t LinkedList.
+ * @head: pointer to first node
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	if (head == NULL)
+		return;
+	while (head)
+	{
+		node = head->next;
+		free(head);
+		head = node;
+	}
+	head = NULL;
+}
