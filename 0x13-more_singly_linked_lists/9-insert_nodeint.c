75e7a608cc3056a275e1e744cfc2da87a4fffdfe
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-04-04 13:30:51 +0200

    Task 9 fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,26 +1,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
-/**
- * listint_len - count all nodes of a LinkedList
- * @head: head of Linked List
- *
- * Return: size of list
- */
-size_t listint_len(const listint_t *head)
-{
-	size_t count = 0;
-
-	while (head)
-	{
-		count++;
-		head = head->next;
-	}
-
-	return (count);
-}
-
 /**
  * insert_nodeint_at_index - add new node at an index in LinkedList
  * @head: pointer to the head of Linked List
@@ -34,26 +14,37 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = (listint_t *) malloc(sizeof(listint_t));
 	listint_t *node = *head;
-	unsigned int at = 0, len = listint_len(*head);
+	unsigned int at = 0;
 
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 
-	if (idx >= len)
+	if (head == NULL)
 		return (NULL);
 
-	while (at != (idx - 1))
+	if (idx == 0)
 	{
-		node = node->next;
-		at++;
+		new_node->next = node;
+		*head = new_node;
+		return (new_node);
 	}
 
-	new_node->next = node->next;
-	node->next = new_node;
+	/*move to idx of LinkedList*/
+	while (node && node->next)
+	{
+		at++;
+		if (idx == at)
+		{
+			new_node->next = node->next;
+			node->next = new_node;
+			return (new_node);
+		}
+		node = node->next;
+	}
 
-	return (node);
+	return (NULL);
 }
 
 
