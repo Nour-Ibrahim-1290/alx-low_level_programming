49acf9d68f12d04fb1d7c1f616c56007f729aa12
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-21 00:46:26 +0200

    0x17:task 06

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - get sum of all values of a double linkedlist
+ * @head: head of Linked List
+ *
+ * Return: int sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
