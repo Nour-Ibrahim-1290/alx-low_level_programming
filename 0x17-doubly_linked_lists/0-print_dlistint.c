bc6fa15ef8cd357fae400b292497ace299c033f1
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-20 02:05:38 +0200

    0x17:task 00

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print all nodes of a DoublyLinkedList
+ * strting with head to tail nodes
+ * @h: head of Linked List
+ *
+ * Return: size of list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d \n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
