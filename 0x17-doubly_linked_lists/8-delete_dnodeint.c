c1c478e49cd90fe35ca555a52505e152a2c4bf74
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-05-21 20:16:57 +0200

    0x17-task 08

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - get node at a predefined index
+ * @h: head of Linked List
+ * @idx: index to retrive the node from
+ *
+ * Return: dnode
+ */
+
+int delete_dnodeint_at_index(dlistint_t **h, unsigned int idx)
+{
+	unsigned int count = 0;
+	int flag = -1;
+	dlistint_t *head;
+
+	if (*h == NULL)
+		return (flag);
+	head = *h;
+	if (idx == 0)
+	{
+		*h = head->next;
+		if (*h != NULL)
+		{
+			(*h)->prev = NULL;
+			flag = 1;
+		}
+	}
+	else
+	{
+		while (head->prev != NULL) /*make sure I'm at head*/
+			head = head->prev;
+		while (head)
+		{
+			if (idx == count)
+			{
+				if (head->next == NULL)
+					head = NULL;
+				else
+				{
+					head->prev->next = head->next;
+					head->next->prev = head->prev;
+					head = NULL;
+				}
+				flag = 1;
+				break;
+			}
+			count++;
+			head = head->next;
+		}
+	}
+	return (flag);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -27,5 +27,6 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int idx);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int idx);
 
 #endif
