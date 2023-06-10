d11f9370758ca568aa33c07add0982ed0dde4738
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-06-11 01:43:16 +0200

    0x1A-C task01

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,7 +36,7 @@ typedef struct hash_table_s
 
 /* hash data structure functions*/
 hash_table_t *hash_table_create(unsigned long int size);
-
+unsigned long int hash_djb2(const unsigned char *str);
 
 
 
