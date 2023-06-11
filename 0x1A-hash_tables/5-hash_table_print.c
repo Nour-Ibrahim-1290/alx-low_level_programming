99acb2946f4bb531a90bbe616ae0abc0c460feb9
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2023-06-11 05:31:23 +0200

    0x1A-C task05

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints all elemets in hash table
+ * @ht: hash table to print
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int counter;
+	hash_node_t *temp;
+	unsigned int flag = 0;
+
+	if ((!ht) || (ht->size == 0))
+		printf("{}");
+
+	printf("{");
+	for (counter = 0; counter < ht->size; counter++)
+	{
+		temp = ht->array[counter];
+		while (temp != NULL)
+		{
+			printf("'%s': '%s'", temp->key, temp->value);
+			temp = temp->next;
+			if (temp)
+				printf("in, ");
+		}
+
+		if (ht->array[counter] != NULL)
+			flag = 1;
+		if (flag == 1  && (ht->array[counter + 1] != NULL))
+			printf(", ");
+	}
+	printf("}\n");
+}
